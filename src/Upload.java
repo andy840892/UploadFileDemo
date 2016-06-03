45ad2efda65b0e6b5bbd9800da10617949313996
andy840892 <andy840892@gmail.com>
2016-06-03 12:26:36 +1000

    Delete service updates.

--- src/Upload.java
+++ src/Upload.java
@@ -13,13 +13,16 @@ import org.json.simple.JSONObject;
 public class Upload extends HttpServlet {
 
 	private String message;
+	private String cdapath;
 
 	public void init() throws ServletException {
 		// Do required initialization
 		message = "Hello World";
+		cdapath="C:\\Program Files\\Apache Group\\Tomcat 4.1\\webapps\\osr\\CDAfiles\\";
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		System.out.println("Runnning upload function. DOGET");
 		// Set response content type
 
@@ -34,7 +37,7 @@ public class Upload extends HttpServlet {
 
 		// write the inputStream to a FileOutputStream
 		OutputStream outputStream = new FileOutputStream(
-				new File("C:\\Program Files\\Apache Group\\Tomcat 4.1\\webapps\\osr\\Pictures\\"+fileName));
+				new File(cdapath+fileName));
 		System.out.println("See if we can get the steam size " + fileContent.toString());
 		int read = 0;
 		byte[] bytes = new byte[1024];
@@ -61,6 +64,7 @@ public class Upload extends HttpServlet {
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		System.out.print("Start uploading file code do post function. ");
 		// Set response content type
 		/*
@@ -72,75 +76,103 @@ public class Upload extends HttpServlet {
 		 * 
 		 * });
 		 */
-
+		String action = request.getParameter("action");
+		System.out.println("The action name of the post is " + action);;
 		try {
 
-			Part filePart = request.getPart("files[]"); // Retrieves <input
-														// type="file"
-														// name="file">
-			String fileName = filePart.getSubmittedFileName();
-			String filesize = String.valueOf(filePart.getSize());
-			 
-		 
-			InputStream fileContent = filePart.getInputStream();
-
-			System.out.println("See if we can get the filename here " + fileName);
-
-			// write the inputStream to a FileOutputStream
-			OutputStream outputStream = new FileOutputStream(
-					new File("C:\\Program Files\\Apache Group\\Tomcat 4.1\\webapps\\osr\\Pictures\\"+fileName));
-			System.out.println("See if we can get the steam size " + fileContent.toString());
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			while ((read = fileContent.read(bytes)) != -1) {
-				outputStream.write(bytes, 0, read);
-			}
-			fileContent.close();
-			outputStream.close();
-			// response.setContentType("text/html");
-			response.setContentType("application/json");
-			response.setCharacterEncoding("UTF-8");
-			// Get the printwriter object from response to write the required
-			// json object to the output stream
-			PrintWriter out = response.getWriter();
+			String Servletname = "http://tj8.trisearch.com.au";
 
 			JSONObject obj = new JSONObject();
-		//	obj.put("files", "");
+			// obj.put("files", "");
 			JSONObject file = new JSONObject();
+			if (action.equalsIgnoreCase("upload")) {
+				Part filePart = request.getPart("files[]"); // Retrieves <input
+				// type="file"
+				// name="file">
+				String fileName = filePart.getSubmittedFileName();
+				String filesize = String.valueOf(filePart.getSize());
 
-			file.put("name", fileName);
-			file.put("size", filesize);
-			file.put("url", "upload?getfile=" );
-			file.put("thumbnail_url", "upload?getthumb=" );
-			file.put("deleteUrl", "http://example.org/files/picture1.jpg");
-			file.put("deleteType", "DELETE");
-			JSONArray filelist = new JSONArray();
-			filelist.add(file);
-		 
-			obj.put("files", filelist);
-			
-			
+				InputStream fileContent = filePart.getInputStream();
+
+				System.out.println("See if we can get the filename here " + fileName);
+
+				// write the inputStream to a FileOutputStream
+				OutputStream outputStream = new FileOutputStream(
+						new File(cdapath + fileName));
+				System.out.println("See if we can get the steam size " + fileContent.toString());
+				int read = 0;
+				byte[] bytes = new byte[1024];
+
+				while ((read = fileContent.read(bytes)) != -1) {
+					outputStream.write(bytes, 0, read);
+				}
+				fileContent.close();
+				outputStream.close();
+				// response.setContentType("text/html");
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8");
+				// Get the printwriter object from response to write the
+				// required
+				// json object to the output stream
+				PrintWriter out = response.getWriter();
+
+
+				file.put("name", fileName);
+				file.put("size", filesize);
+				file.put("url", Servletname + "/osr/Pictures/" + fileName);
+				file.put("thumbnail_url", "upload?getthumb=");
+				file.put("deleteUrl", Servletname + "/upload/upload?action=delete&filename=" + fileName);
+				file.put("deleteType", "DELETE");
+				JSONArray filelist = new JSONArray();
+				filelist.add(file);
+
+				obj.put("files", filelist);
+
+				// Assuming your json object is **jsonObject**, perform the
+				// following, it will return your json object
+				out.print(obj);
+				System.out.println("what can we find from the json" + obj.toJSONString());
+				out.flush();
+			}
 			/*
-			 * String jsonresponse =
-			 * 
-			 * {"files": [ { "name": "picture1.jpg", "size": 902604, "url":
-			 * "http:\/\/example.org\/files\/picture1.jpg", "thumbnailUrl":
-			 * "http:\/\/example.org\/files\/thumbnail\/picture1.jpg",
-			 * "deleteUrl": "http:\/\/example.org\/files\/picture1.jpg",
-			 * "deleteType": "DELETE" }, { "name": "picture2.jpg", "size":
-			 * 841946, "url": "http:\/\/example.org\/files\/picture2.jpg",
-			 * "thumbnailUrl":
-			 * "http:\/\/example.org\/files\/thumbnail\/picture2.jpg",
-			 * "deleteUrl": "http:\/\/example.org\/files\/picture2.jpg",
-			 * "deleteType": "DELETE" } ]}"
-			 */
+			if (action.equalsIgnoreCase("deletes")) {
+			 
+				String deletefileName = request.getParameter("filename");  
 
-			// Assuming your json object is **jsonObject**, perform the
-			// following, it will return your json object
-			out.print(obj);
-			System.out.println("what can we find from the json" + obj.toJSONString());
-			out.flush();
+				System.out.println("See if we can get the filename here " + deletefileName);
+				File deletefile = new File(cdapath+ deletefileName);
+	        	
+	    		if(deletefile.delete()){
+	    			System.out.println(cdapath+deletefile.getName() + " is deleted!");
+
+					file.put(deletefileName, "true"); 
+	    		}else{
+	    			System.out.println("Delete operation is failed.");
+	    			file.put(deletefileName, "false"); 
+					//file.put("name", deletefileName);
+					//file.put("size", deletefile);
+					//file.put("error", "Error happened when deleting the file."); 
+	    		}
+	    	    
+				response.setContentType("application/json");
+				response.setCharacterEncoding("UTF-8"); 
+				PrintWriter out = response.getWriter();
+ 
+
+				JSONArray filelist = new JSONArray();
+				filelist.add(file);
+
+				obj.put("files", filelist);
+
+				// Assuming your json object is **jsonObject**, perform the
+				// following, it will return your json object
+				out.print(obj);
+				System.out.println("what can we find from the json" + obj.toJSONString());
+				out.flush();
+			}
+			*/
+			
+			
 			// Actual logic goes here.
 			// PrintWriter out = response.getWriter();
 			// out.println("<h1>" + message + "</h1>");
@@ -149,7 +181,60 @@ public class Upload extends HttpServlet {
 
 		}
 	}
+	public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.print("Start uploading file code do delete function. ");
+		//response.setHeader("Access-Control-Allow-Origin", "*");
+	 
+		String action = request.getParameter("action");
+		System.out.println("The action name of the post is " + action);;
+		try {
 
+			String Servletname = "http://tj8.trisearch.com.au";
+
+			JSONObject obj = new JSONObject();
+			// obj.put("files", "");
+			JSONObject file = new JSONObject();
+			 
+			if (action.equalsIgnoreCase("delete")) {
+			 
+				String deletefileName = request.getParameter("filename");  
+
+				System.out.println("See if we can get the filename here " + deletefileName);
+				File deletefile = new File(cdapath + deletefileName);
+	        	
+	    		if(deletefile.delete()){
+	    			System.out.println(deletefile.getName() + " is deleted!");
+
+					file.put(deletefileName, "true"); 
+					response.setContentType("application/json");
+					response.setCharacterEncoding("UTF-8"); 
+					PrintWriter out = response.getWriter();
+	 
+
+					JSONArray filelist = new JSONArray();
+					filelist.add(file);
+
+					obj.put("files", filelist); 
+					out.print(obj);
+					System.out.println("what can we find from the json" + obj.toJSONString());
+					out.flush();
+	    		}else{
+	    			System.out.println("Delete operation is failed.");
+	    			//file.put(deletefileName, "false"); 
+					//file.put("name", deletefileName);
+					//file.put("size", "76085");
+					//file.put("error", "Error happened when deleting the file."); 
+	    		}
+	    	    
+			
+			}
+			
+			 
+		} catch (Exception e) {
+			System.out.println( "error "+e.toString());
+
+		}
+	}
 	public void destroy() {
 		// do nothing.
 	}
